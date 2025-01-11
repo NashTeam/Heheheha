aa2ca9d4868b101bdbbfa7935488a91a61bea66a
NashTeam <azkaputri270@gmail.com>
2025-01-11 20:17:59 +0700

    Add files via upload

--- system/models/user.js
+++ system/models/user.js
@@ -1,1 +1,15 @@
-y
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  googleId: String,
+  username: String, // Pastikan username ada di skema
+  email: String,
+  limit: Number,
+  profile: String,
+  apiKey: String,
+  premium: Boolean,
+  premiumTime: Number,
+  defaultKey: String,
+});
+
+module.exports = mongoose.model('User', userSchema);
